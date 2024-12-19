1d698781836528362a1aa1dbdaee592cfe9db38e
Estevao Henrique <ehp762@gmail.com>
2024-12-19 20:40:49 -0300

    19/12/2024 - Compactando / Descompactando arquivos com zipfile.ZipFile

---
+++ curso_python/aula_intermediaria/aulaintermediaria18.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+from pathlib import Path
+from zipfile import ZipFile
+
+
+CAMINHO_RAIZ = Path(__file__).parent
+CAMINHO_ZIP_DIR = CAMINHO_RAIZ / 'aulaintermediaria18_diretorio_zip'
+CAMINHO_COMPACTADO = CAMINHO_RAIZ / 'aulaintermediaria18_compactado_zip'
+CAMINHO_DESCOMPACTADO = CAMINHO_RAIZ / 'aulaintermediaria18_descompactado'
+
+shutil.rmtree(CAMINHO_ZIP_DIR, ignore_errors=True)
+Path.unlink(CAMINHO_COMPACTADO, missing_ok=True)
+shutil.rmtree(str(CAMINHO_COMPACTADO).replace('.zip', ''), ignore_errors=True)
+shutil.rmtree(CAMINHO_DESCOMPACTADO, ignore_errors=True)
+
+#Cria diretorio
+CAMINHO_ZIP_DIR.mkdir(exist_ok=True)
+
+
+def criar_arquivos(qtd:int, zip_dir: Path):
+    for i in range(qtd):
+        texto = 'arquivo_%s' % i
+        with open(zip_dir / f'{texto}.txt', 'w') as arquivo:
+            arquivo.write(texto)
+
+
+criar_arquivos(10, CAMINHO_ZIP_DIR)
+
+with ZipFile(CAMINHO_COMPACTADO, 'W') as zip:
+    for root, dirs, files in os.walk(CAMINHO_ZIP_DIR):
+        for file in files:
+            print(file)
+            zip.write(os.path.join(root, file), file)
+
+
+with ZipFile(CAMINHO_COMPACTADO, 'r') as zip:
+    for arquivo in zip.namelist():
+        print(arquivo)
+
+with ZipFile(CAMINHO_COMPACTADO, 'r') as zip:
+    zip.extractall(CAMINHO_DESCOMPACTADO)
